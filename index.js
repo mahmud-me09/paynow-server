51195a94c8988f3754ee1385aff9ec6046e248da
mahmud-me09 <mahmud.me09@gmail.com>
2024-07-18 08:29:29 +0600

    login section completed

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 require("dotenv").config();
 const app = express();
 const jwt = require("jsonwebtoken");
-const bycript = require("bcryptjs");
+const bcrypt = require("bcryptjs");
 const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion } = require("mongodb");
 
@@ -36,37 +36,122 @@ async function run() {
 			"Pinged your deployment. You successfully connected to MongoDB!"
 		);
 
-		// User Login Section
-		
+		// jwt related
 
+		app.post("/jwt", async (req, res) => {
+			const user = req.body;
+			const token = jwt.sign(user, process.env.Secret, {
+				expiresIn: "7d",
+			});
+		});
+
+		const verifyToken = (req, res, next) => {
+			if (!req.headers.authorization) {
+				return res.status(401).send({ message: "unauthorized access" });
+			}
+			const token = req.headers.authorization.split(" ")[1];
+			jwt.verify(token, process.env.Secret, (err, decoded) => {
+				if (err) {
+					return res
+						.status(401)
+						.send({ message: "unauthorized access" });
+				}
+				req.decoded = decoded;
+				next();
+			});
+		};
+
+		// use verify admin after verifyToken
+		const verifyAdmin = async (req, res, next) => {
+			const email = req.decoded.email; //need to fix
+			const query = { email: email }; //need to fix
+			const user = await userCollection.findOne(query);
+			const isAdmin = user?.role === "admin";
+			if (!isAdmin) {
+				return res.status(403).send({ message: "forbidden access" });
+			}
+			next();
+		};
+
+		// User Login Section
+
+		app.post("/login", async (req, res) => {
+			try {
+				const credentials = req.body;
+				const user = await userCollection.findOne({
+					$or: [
+						{ email: credentials.id },
+						{ number: credentials.id },
+					],
+				});
+
+				if (!user) {
+					res.send({
+						message: "email or number is not Found",
+						status: 401,
+					});
+				}
+				const isPinMatch = await bcrypt.compare(req.body.pin, user.pin);
+				console.log(isPinMatch);
+
+				if (!isPinMatch) {
+					res.send({
+						message: "Wrong Credentials",
+						status: 401,
+					});
+				}
+
+				res.send({
+					message: "User Login Granted",
+					status: 200,
+				});
+			} catch (error) {
+				console.log(error);
+				res.send({ message: "error happened", status: 401 });
+			}
+		});
 		// User Registration Section
 
 		app.post("/registration", async (req, res) => {
-			const credentials = {
-				name: req.body.name,
-				email: req.body.email,
-				number: req.body.number,
-				pin: req.body.pin,
-				isAgent: false,
-				isAdmin: false,
-				balance: 0,
-				transactionHistory: [],
-			};
-			const existingUser = await userCollection.findOne({
-				$or: [
-					{ email: credentials.email },
-					{ number: credentials.number },
-				],
-			});
-			if (existingUser) {
-				res.send({message:"User already registered", status:409});
-			} else {
-				const result = await userCollection.insertOne(credentials);
-				res.send(result);
+			try {
+				const credentials = {
+					name: req.body.name,
+					email: req.body.email,
+					number: req.body.number,
+					pin: req.body.pin,
+					isAgent: false,
+					isAdmin: false,
+					balance: 0,
+					transactionHistory: [],
+				};
+				const existingUser = await userCollection.findOne({
+					$or: [
+						{ email: credentials.email },
+						{ number: credentials.number },
+					],
+				});
+				if (existingUser) {
+					res.send({
+						message: "User already registered",
+						status: 409,
+					});
+				} else {
+					// bcrypt for hashing pin
+					const saltRounds = 10;
+					const hashedPin = await bcrypt.hash(
+						credentials.pin,
+						saltRounds
+					);
+
+					credentials.pin = hashedPin;
+
+					const result = await userCollection.insertOne(credentials);
+					res.send(result);
+				}
+			} catch {
+				res.send({ message: "error happened", status: 403 });
 			}
 		});
-
-
 	} finally {
 	}
 }
